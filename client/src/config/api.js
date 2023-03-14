71889eefb2766d9a4567a7f91232236194237d29
the-spatializer <74182913+the-spatializer@users.noreply.github.com>
2023-03-14 15:07:02 +0700

    deploy setup

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 'waysbeans-coffee.up.railway.app/api/v1/',
+  baseURL: 'https://waysbeans-coffee.up.railway.app/api/v1/',
 });
 
 export const setAuthToken = (token) => {
