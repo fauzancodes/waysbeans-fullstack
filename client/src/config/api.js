ca9a1cb3440e3bc9b45863ee71fbafae8481af86
the-spatializer <74182913+the-spatializer@users.noreply.github.com>
2023-03-17 08:34:47 +0700

    activate base url

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  // baseURL: process.env.REACT_APP_BASE_URL,
-  baseURL: "https://waysbeans-coffee.up.railway.app/api/v1/",
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 export const setAuthToken = (token) => {
