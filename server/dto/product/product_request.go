65bb3550b3dda171e588616556a1e76b1f829b73
Fauzan | Fullstack Developer <mfauzan.murtadho@gmail.com>
2024-06-09 20:13:18 +0700

    remove required in image request

--- server/dto/product/product_request.go
+++ server/dto/product/product_request.go
@@ -4,6 +4,6 @@ type ProductRequest struct {
 	Name        string `json:"name" form:"name" validate:"required"`
 	Description string `json:"description" form:"description" validate:"required"`
 	Price       int    `json:"price" form:"price" validate:"required"`
-	Photo       string `json:"photo" form:"photo" validate:"required"`
+	Photo       string `json:"photo" form:"photo"`
 	Stock       int    `json:"stock" form:"stock" validate:"required"`
 }
--- server/dto/profile/profile_request.go
+++ server/dto/profile/profile_request.go
@@ -2,7 +2,7 @@ package profiledto
 
 type ProfileRequest struct {
 	ID      int    `json:"id" gorm:"primary_key:auto_increment" validate:"required"`
-	Photo   string `json:"photo" gorm:"type: varchar(255)" validate:"required"`
+	Photo   string `json:"photo" gorm:"type: varchar(255)"`
 	Phone   string `json:"phone" gorm:"type: varchar(255)" validate:"required"`
 	Address string `json:"address" gorm:"type: text" validate:"required"`
 	UserID  int    `json:"user_id"`
